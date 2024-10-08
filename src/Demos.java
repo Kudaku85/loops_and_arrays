de21e7dbb0071bc129957c72c39f33ffb95a6cfa
Kudaku85 <eivindwe@gmail.com>
2024-10-08 23:34:26 +0200

    Forenklet litt ved å droppe trip-klassen, lagt til kommentarer. Også lagt til alternativ kode for å kunne kjøre programmet raskt flere ganger.

--- src/Demos.java
+++ src/Demos.java
@@ -3,22 +3,26 @@ import java.util.Random;
 
 public class Demos {
     public static void main(String[] args) {
-        Random rand = new Random();
+        Random rand = new Random(); //Vi lager et objekt av klasse Random, som vi bruker for å generere tilfeldige tall.
         DecimalFormat formatter = new DecimalFormat("#, ###"); //Brukes for å endre tallformatet på store integers til x,xxx,xxx repeating
-        int randomNumber = 0;
-        int attempt = 0;
+        int randomNumber = 0; //tallet må initialiseres utenfor loopen, ellers vil compileren få problemer.
+        int attempt = 0; //Se over
 
-        while (randomNumber != 1) {
-            randomNumber = rand.nextInt(0, 5_379_617); //Ops, bound er _til_, ikke _til og med_. Bound-verdien må derfor være 1 høyere enn maks-verdien vi vil ha.
-            attempt++;
+
+        //while-Loop som lager random tall og sjekker om de matcher et bestemt tall.
+        while (randomNumber != 1) { //Vil kjøre så lenge tallet ikke er nøyaktig 1.
+            //Under genererer vi tall mellom origin (0) og bound (5 379 617). Oddsene for å vinne i lotto er 1 til 5 379 616
+            randomNumber = rand.nextInt(0, 5_379_617); //Ops, Bound-verdien må være 1 høyere enn maks-verdien vi vil ha.
+            attempt++; //Holder styr på hvor mange forsøk vi trenger.
         }
         System.out.println("Jackpot! It only took "+formatter.format(attempt) + "weeks.");
 
+        //Denne while-loop'en gjør det samme som den over, men er strukturert annerledes. while(true) vil kjøre helt til vi bruker en break command.
         while(true){
-            randomNumber = rand.nextInt(0, 5_379_617);
+            randomNumber = rand.nextInt(0, 5_379_617); //Genererer random tall mellom origin og bound.
             attempt++;
-            if(randomNumber == 7){
-                break;
+            if(randomNumber == 7){ //Sjekker om vi har funnet riktig tall.
+                break; //Bryter ut av loopen
             }
         }
         System.out.println("Jackpot! This time it only took "+formatter.format(attempt) + "weeks.");
--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,57 @@
 import static javax.swing.JOptionPane.showInputDialog;
 
-class Trip {
-    String departure;
-    String destination;
-    int planeCapacity;
-
-    public Trip(String departure, String destination, int planeCapacity) {
-        this.departure = departure;
-        this.destination = destination;
-        this.planeCapacity = planeCapacity;
-    }
-}
-
+//Klasse for å opprette billetter
 class Ticket {
     String name;
-    Trip trip;
+    String ticketStatus;
+    String destination = "Milan";
+    String departure = "Oslo";
 
-    public Ticket(String name, Trip trip) {
+    //Konstruktør for å enkelt opprette objekter av billetter
+    public Ticket(String name, String ticketStatus) {
         this.name = name;
-        this.trip = trip;
+        this.ticketStatus = ticketStatus;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Trip tripToMilan = new Trip("Oslo", "Milan", 5);
-        Ticket[] passengerList = new Ticket[tripToMilan.planeCapacity];
-        int milanTicketsSold = 0;
+        int planeCapacity = 5; //Dette bestemmer hvor mange som får plass på flyet, og er maksverdien for loopen vår.
+        Ticket[] passengerList = new Ticket[planeCapacity]; //Vi oppretter et array med antall plasser lik flykapasiteten
+        int ticketCount = 0; //Initialiserer verdien utenfor loopen.
+        //String[] names = {"Eivind", "Sonja", "Siri", "Kaja", "Lars"}; //Aktiver kodelinje 22 og 25 og kommenter ut linje 26 hvis dere vil slippe input.
 
-                while (milanTicketsSold < tripToMilan.planeCapacity) {
-            String name = showInputDialog("Hva heter passasjeren?");
-            if (!name.isBlank()) {
-                Ticket freshTicket = new Ticket(name, tripToMilan);
-                passengerList[milanTicketsSold] = freshTicket;
-                milanTicketsSold++;
+        while (ticketCount < planeCapacity) { //Starter en while-loop.
+            //String name = names[ticketCount];
+            String name = showInputDialog("Hva heter passasjeren?"); //Kommenter ut denne kodelinjen og aktiver linje 22 & 25 hvis dere blir lei av å skrive inn navn.
+            if (!name.isBlank()) { //Sjekker om navnet er blankt. Hvis det er det trigges ikke if-funksjonen og loopen begynner på nytt.
+                Ticket freshTicket = new Ticket(name, "gyldig"); //Hvis navnet ikke er blankt bruker vi det til å opprette en ny billett
+                passengerList[ticketCount] = freshTicket; //og legger billetten i arrayet, på posisjon = ticketSsold.
+                ticketCount++; //Så øker vi telleren vår og posisjonen i arrayet.
             }
         }
-        for (int i = 0; i < tripToMilan.planeCapacity; i++){
-            System.out.println(passengerList[i]);
 
+//        for (int i = 0; i < tripToMilan.planeCapacity; i++){
+//            System.out.println(passengerList[i]); //Obs, vil kun printe ut minneadressen til objektet, ikke innholdet. Lærer mer om dette senere.
+//        }
+
+        for (int i = 0; i < planeCapacity; i++){//Denne loopen vil printe ut informasjonen fra hver enkelt billett.
+            System.out.println(passengerList[i].name +" er på vei til " + passengerList[i].destination + " med avreise fra " + passengerList[i].departure);
         }
 
-        for (Ticket ticket : passengerList) {
-            System.out.println(ticket.name +" er på vei til " + ticket.trip.destination + " med avreise fra " + ticket.trip.departure);
+        System.out.println("\nflyet ditt er omdirigert pga tåke elns lol. Hilsen RyanAir \n");
+
+        for (Ticket ticket : passengerList) { //Bruker en for loop for å endre destinasjonen for hver enkelt billett
+            ticket.destination = "Schiphol (!)";
+            System.out.println(ticket.name +" er nå på vei til " + ticket.destination);
         }
-
-        tripToMilan.destination = "Schiphol (Faen!)";
-        System.out.println("flyet ditt er omdirigert pga tåke elns lol");
-
-        for (Ticket ticket : passengerList) {
-            System.out.println(ticket.name +" er på vei til " + ticket.trip.destination + " med avreise fra " + ticket.trip.departure);
+        System.out.println();
+        for (Ticket ticket : passengerList){ //Vi kan også bruke loops til å lete gjennom et array og kun endre enkelte verdier.
+            if (ticket.name.equals("Eivind")) { //Sjekker om vi har funnet riktig passasjer
+                ticket.destination = "Norilsk, Siberia"; //...Og gir han en skikkelig dårlig dag.
+                System.out.println(ticket.name +" er nå på vei til " + ticket.destination);
+                break;
+            }
         }
     }
 }
