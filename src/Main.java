17546dba0d0c51289cc64be4957072637dad5a60
Kudaku85 <eivindwe@gmail.com>
2024-10-09 09:53:56 +0200

    Siste tweak, fjernet ticketStatus-verdi og forenklet søkefunksjonen.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ import static javax.swing.JOptionPane.showInputDialog;
 //Klasse for å opprette billetter
 class Ticket {
     String name;
-    String ticketStatus;
     String destination = "Milan";
     String departure = "Oslo";
 
     //Konstruktør for å enkelt opprette objekter av billetter
-    public Ticket(String name, String ticketStatus) {
+    public Ticket(String name) {
         this.name = name;
-        this.ticketStatus = ticketStatus;
     }
 }
 
@@ -24,8 +22,8 @@ public class Main {
         while (ticketCount < planeCapacity) { //Starter en while-loop.
             //String name = names[ticketCount];
             String name = showInputDialog("Hva heter passasjeren?"); //Kommenter ut denne kodelinjen og aktiver linje 22 & 25 hvis dere blir lei av å skrive inn navn.
-            if (!name.isBlank()) { //Sjekker om navnet er blankt. Hvis det er det trigges ikke if-funksjonen og loopen begynner på nytt.
-                Ticket freshTicket = new Ticket(name, "gyldig"); //Hvis navnet ikke er blankt bruker vi det til å opprette en ny billett
+            if (!name.isBlank()) { //Sjekker om navnet er blankt. Hvis det er det trigges ikke if-funksjonen og loopen begynner på nytt uten å øke ticketCount.
+                Ticket freshTicket = new Ticket(name); //Hvis navnet ikke er blankt bruker vi det til å opprette en ny billett
                 passengerList[ticketCount] = freshTicket; //og legger billetten i arrayet, på posisjon = ticketSsold.
                 ticketCount++; //Så øker vi telleren vår og posisjonen i arrayet.
             }
@@ -50,7 +48,7 @@ public class Main {
             if (ticket.name.equals("Eivind")) { //Sjekker om vi har funnet riktig passasjer
                 ticket.destination = "Norilsk, Siberia"; //...Og gir han en skikkelig dårlig dag.
                 System.out.println(ticket.name +" er nå på vei til " + ticket.destination);
-                break;
+                break; //break kan også brukes for å avslutte en for-loop tidlig. Vi trenger ikke kjøre resten av loop'en når vi har funnet ̶o̶f̶f̶e̶r̶e̶t̶ passasjeren vår.
             }
         }
     }
